4ff78402aed2f424704d6f8bf18a0c3bd542c635
Rafael Chung <rafaelchung28@hotmail.com>
2024-11-29 11:35:43 -0500

    inteto de Swagger/OpenAPI

--- be/app/__init__.py
+++ be/app/__init__.py
@@ -6,6 +6,9 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+from flask_apispec import FlaskApiSpec
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
 
 APP_ROOT = os.path.join(os.path.dirname(__file__), "..")
 dotenv_path = os.path.join(APP_ROOT, ".env")
@@ -15,8 +18,21 @@ app = Flask(__name__)
 
 enviroment = config["ambiente"]
 app.config.from_object(enviroment)
-db = SQLAlchemy(app)
 
+# Configuración Swagger
+app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='Pokemon API',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/',
+    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'
+})
+
+db = SQLAlchemy(app)
+docs = FlaskApiSpec(app)
 Migrate(app, db)
 CORS(app, supports_credentials=True)
 
