068a9c0b2aba403c7e219013971b9e956fc2de76
Rafael Chung <rafaelchung28@hotmail.com>
2024-11-29 02:19:23 -0500

    cambio de front

--- fe/src/app/services/api.config.js
+++ fe/src/app/services/api.config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const ApiService = axios.create({
     // baseURL: 'http://localhost:8080/', // PRODUCTION BACKEND URL
-    baseURL: 'http://localhost:8080/', // DEV BACKEND URL
+    baseURL: 'http://192.168.0.25:5001', // DEV BACKEND URL
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
