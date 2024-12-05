95efe8358f615f6b4153c49b89e2c8f7c67e7b4b
Rafael Chung <rafaelchung28@hotmail.com>
2024-12-05 17:03:28 -0500

    Update __init__.py

--- be/app/__init__.py
+++ be/app/__init__.py
@@ -1,4 +1,3 @@
-from functools import wraps
 import os
 from flask import Flask
 from app.config import config
@@ -11,12 +10,9 @@ from dotenv import load_dotenv
 APP_ROOT = os.path.join(os.path.dirname(__file__), "..")
 dotenv_path = os.path.join(APP_ROOT, ".env")
 load_dotenv(dotenv_path)
-
 app = Flask(__name__)
 enviroment = config["ambiente"]
 app.config.from_object(enviroment)
-
-
 db = SQLAlchemy(app)
 Migrate(app, db)
 CORS(app, supports_credentials=True)
