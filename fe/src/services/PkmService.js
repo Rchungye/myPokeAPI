ea1ac843d7b324794f29612a9e5cb9d5ff08e9ff
Rafael Chung <rafaelchung28@hotmail.com>
2024-12-15 15:42:35 -0500

    Update PkmService.js

--- fe/src/services/PkmService.js
+++ fe/src/services/PkmService.js
@@ -1,9 +1,8 @@
 import { ApiService } from './api.config'
 
 export const GetAllPokemon = async () => {
-    return ApiService.get('/pokemon/all')
-      .then((response) => response.data)
-      .catch((response) => Promise.resolve(response))
-  }
+  return ApiService.get('/pokemon/all')
+    .then((response) => response.data)
+    .catch((response) => Promise.resolve(response))
+}
 
-  
